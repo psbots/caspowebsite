db0bc9fa72c0c476cf019891cf26728edf6163c7
Praveen Sridhar <prvn431@gmail.com>
2015-08-06 20:07:24 +0530

    beta site
    added a new beta site at /beta

---
+++ beta/js/ani.js
@@ -0,0 +1,113 @@
+//Disable Scrolling 
+
+function startSceneAnimations() {
+
+	var lastPosition = -100;
+
+$('#start-stove-img').css({
+	    "transform":"translateY(0%)"
+	  	}); 
+
+
+$('#headline-texts').css({
+	    "transform":"translateY(-600%)",
+	    "opacity" : 1
+	  	}); 
+
+$('body').css('overflow', 'scroll');
+
+
+
+
+$(function(){
+	
+	var $window = $(window);		//Window object
+	
+	var scrollTime = 0.4;			//Scroll time
+	var scrollDistance = 325;		//Distance. Use smaller value for shorter scroll and greater value for longer scroll
+	
+	$window.on("mousewheel DOMMouseScroll", function(event){
+		
+		event.preventDefault();	
+										
+		var delta = event.originalEvent.wheelDelta/120 || -event.originalEvent.detail/3;
+		var scrollTop = $window.scrollTop();
+		var finalScroll = scrollTop - parseInt(delta*scrollDistance);
+			
+		TweenMax.to($window, scrollTime, {
+			scrollTo : { y: finalScroll, autoKill:true },
+				ease: Power1.easeOut,	//For more easing functions see http://api.greensock.com/js/com/greensock/easing/package-detail.html
+				autoKill: true,
+				overwrite: 5							
+			});
+					
+	});
+	
+});
+
+//$("html").niceScroll();
+//Jquery Animations
+}
+
+
+$(window).load(function(){
+
+
+		$('.img-slider').bxSlider({
+  auto: true,
+hideControlOnEnd: true,
+controls : false
+});
+
+
+});
+
+
+
+ $(window).ready(function(){
+
+
+ 	$('#scene2').waypoint({
+  handler: function(direction) {
+   
+   $('#s2SideText').css({
+	    "transform":"translate(0%,0%)",
+	    "opacity" : 1
+	  	}); 
+	},
+
+	offset: '18%'
+
+
+});
+
+
+
+
+
+	 $(this).one('mousewheel',function(event){
+	 	window.scrollTo(0,0);
+	 	  $('#start_cover_phone').css({
+	    "transform":"translate(-12%, -100%) scale(0.08) rotate(90deg)"
+	  	}).delay(2000).queue(function (next) { 
+  			  $(this).css({
+	    "transform":"translate(-26%, -91%) scale(0.08) rotate(90deg)"
+	  	}); 
+
+  		startSceneAnimations();
+  			  
+    //next(); 
+  });
+
+	  //	startSceneAnimations();
+	});
+
+	
+
+});
+
+
+
+
+ //Waypoints Setup
+
