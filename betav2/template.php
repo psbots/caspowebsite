8534359955546f630a046ba712ea6b0a2eaf1b90
Athul Suresh <atulsuresh123@gmail.com>
2015-10-19 19:47:11 +0530

    minor patches + php started

---
+++ betav2/template.php
@@ -0,0 +1,69 @@
+<?php 
+
+//Caspo Energy Page Template
+
+?>
+
+
+<html>
+
+<?php
+
+	//include <head>
+	include_once('templates/header.php');
+
+?>
+
+<body id="">
+
+	<section class="hero main-full-window single-page--hero--electric" id="intro">
+        <section class="navigation">
+            
+        	<?php
+
+			//menu-elements
+
+        	include_once('templates/menu_default.php');
+
+        	?>
+
+
+             <div class="addition-nav">
+
+                <div class="a-nav--container">
+                    <a href="#">Design</a>
+                    <a href="#">Features</a>
+                    <a href="#">Features</a>
+                </div>
+             </div>
+        </section>
+
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-md-push-1">
+                    <div class="hero-content text-center">
+                        <h1>Cooking More techie</h1>
+                        <p class="intro">Introducing the new caspo electric induction cooktop powered by blah blah.</p>
+                        
+                    </div>
+
+                </div>
+
+
+<!--
+                <div class="col-md-6">
+                    <img class="hero-content img-responsive" src="img/caspo-cooktop1.jpg" />
+                </div>
+
+ -->
+            </div>
+        </div>
+        <div class="down-arrow floating-arrow"><a href="#"><i class="fa fa-angle-down"></i></a></div>
+
+    </section>
+
+
+
+</body>
+</html>
