9b03feff72446afe7cb62bcde2c3ad9a89920708
Athul Suresh <atulsuresh123@gmail.com>
2015-10-21 06:32:40 +0530

    minor changes

--- betav2/templates/header.php
+++ betav2/templates/header.php
@@ -14,7 +14,7 @@ Caspo Header-- head tag Page
     <meta charset="utf-8">
     <!--<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">-->
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <title>Caspo Eneergy</title>
+    <title>Caspo Energy</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="apple-touch-icon" href="apple-touch-icon.png">
--- betav2/templates/menu_default.php
+++ betav2/templates/menu_default.php
@@ -12,7 +12,7 @@ Default Menu
 
 			<header>
                 <div class="header-content">
-                    <div class="logo"><a href="#"><img src="imgs/caspo-logo.png" width="100px" alt="Caspo logo"></a></div>
+                    <div class="logo"><a href="index.php"><img src="imgs/caspo-logo.png" width="100px" alt="Caspo logo"></a></div>
                     <div class="header-nav">
                         <nav>
                             <ul class="primary-nav">
@@ -24,7 +24,7 @@ Default Menu
                             </ul>
                             <ul class="member-actions">
                                 <li><a href="https://caspoenergy.wordpress.com/" class="login">Blog</a></li>
-                                <li><a href="appshow.php" class="btn-white btn-small">Download the app</a></li>
+                                <li><a href="appshow.php" class="btn btn-white btn-small">Download the app</a></li>
                             </ul>
                         </nav>
                     </div>
